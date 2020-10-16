82c8e8b69e1f13837d30decebe7643c8de545cdc
Joshua MacSween <joshmacsween@gmail.com>
2020-10-16 10:18:16 -0600

    showing products on page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import {Container} from 'react-bootstrap';
-
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
     <>
       <Header />
+      <main className="py-3">
         <Container>
-          <main className="py-3">
-            <h1>Welcome to ProShop</h1>
-          </main>
+          <HomeScreen />
         </Container>
+      </main>
       <Footer />
     </>
   );
-}
+};
 
 export default App;
