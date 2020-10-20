4f0ef37d0aa573616611825d0d2a77789d373730
Joshua MacSween <joshmacsween@gmail.com>
2020-10-20 17:34:08 -0600

    Seeding Data

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -15,7 +15,7 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
     },
-    idAdmin: {
+    isAdmin: {
       type: Boolean,
       required: true,
       default: false,
