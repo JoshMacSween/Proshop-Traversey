de9802642c21af035365fb39493080d521e461d0
Joshua MacSween <joshmacsween@gmail.com>
2020-10-21 08:13:13 -0600

    initialized redux

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -14,7 +15,11 @@ app.get('/', (req, res) => {
   res.send('Api is running')
 })
 
-app.use("/api/products", productRoutes)
+app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
