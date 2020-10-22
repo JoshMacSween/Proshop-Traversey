68e2394b9f52a6a3240a1bfa4cf773c5fae494e5
Joshua MacSween <joshmacsween@gmail.com>
2020-10-21 22:56:12 -0600

    using redux to display data

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
@@ -19,7 +21,7 @@ export const listProducts = () => async (dispatch) => {
       type: PRODUCT_LIST_FAIL,
       payload:
         error.response && error.response.data.message
-          ? error.data.message
+          ? error.response.data.message
           : error.message,
     })
   }
